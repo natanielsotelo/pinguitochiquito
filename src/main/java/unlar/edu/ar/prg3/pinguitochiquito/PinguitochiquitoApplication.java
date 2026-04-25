62417681c2f19385db21e28fbb7db012dbc8193e
byrata <natasoteskr@gmail.com>
2026-04-25 17:25:54 -0300

    pingouramocha

---
+++ src/main/java/unlar/edu/ar/prg3/pinguitochiquito/PinguitochiquitoApplication.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.prg3.pinguitochiquito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinguitochiquitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinguitochiquitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unlar/edu/ar/prg3/pinguitochiquito/PinguitochiquitoApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.prg3.pinguitochiquito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinguitochiquitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
